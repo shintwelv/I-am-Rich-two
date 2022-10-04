1782567d67eb14234fdcb016f203eadb1bb0ca80
elvinHeo <shinilheo0205@gmail.com>
2022-10-04 22:13:54 +0900

    Initial Commit

---
+++ I am Rich two/I_am_Rich_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_twoApp.swift
+//  I am Rich two
+//
+//  Created by ShinIl Heo on 2022/10/04.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
